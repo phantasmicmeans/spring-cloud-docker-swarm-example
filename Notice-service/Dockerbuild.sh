54a176d178e43abf6f51842c740c5153e135e42b
phantasmicmeans <dlaak0630@naver.com>
2018-07-18 10:28:30 +0900

    spring-cloud-netflix-service-notice

--- Notice-service/Dockerbuild.sh
+++ Notice-service/Dockerbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mvn package
-docker build -t phantasmicmeans/notice-service:latest .
-docker push phantasmicmeans/notice-service:latest
+docker build -t {YOUR_DOKCER_ID}/notice-service:latest .
+docker push {YOUR_DOKCER_ID}/notice-service:latest
 
 
